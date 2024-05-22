b7026b67bcc7d803ae86052f7976ca816c0d24ac
unknown <saradawsone@gmail.com>
2024-05-21 23:26:34 -0600

    layout styling on main

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,19 @@ import Header from "./pages/header";
 import Events from "./pages/events";
 import ThingsToKnow from "./pages/thingsToKnow";
 import Ad from "./pages/ads"
+import FAQ from './pages/faq'
 
 export default function Page() {
   return (
     <div>
         <Header></Header>
-        <main>
+        <main className="min-w-content p-20 flex flex-col justify-center">
           <h1></h1>
           <Events></Events>
           <ThingsToKnow></ThingsToKnow>
           <Ad></Ad>
+          <FAQ></FAQ>
         </main>
-
         <footer>
         </footer>
     </div>
