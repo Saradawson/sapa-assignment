fe8dc26372930d68023c52bb74b602a331a0fbe4
unknown <saradawsone@gmail.com>
2024-05-26 17:37:02 -0600

    FAQ page optimized

--- app/pages/faq.jsx
+++ app/pages/faq.jsx
@@ -2,21 +2,21 @@ import Link from "next/link";
 
 export default function FAQ() {
     return (
-        <section className="flex  py-20">
+        <section className="w-full flex py-20">
             <div className="flex flex-col ">
-                <h2 className="text-7xl leading-loose text-greenTwo ">{"FAQs..."}</h2>
-                <div className="flex flex-col ">  
-                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-4xl text-greenThree" >{"If I don’t qualify, do i still have to pay the QMP?"}</Link>
-                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-4xl text-greenThree" >{"What are Utah’s qualifying medical conditions?"}</Link>
-                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-4xl text-greenThree" >{"Is documentation required to prove you have a medical condition?"}</Link>
-                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-4xl text-greenThree" >{"If I have a medical cannabis card, can I consume in public?"}</Link>
-                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-4xl text-greenThree" >{"What are the eligiblity requirements for a Utah medical cannabis card?"}</Link>
-                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-4xl text-greenThree" >{"What is the complete cost to get a medical cannabis card?"}</Link>
-                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-4xl text-greenThree" >{"How long does it take to receive your card?"}</Link>
-                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-4xl text-greenThree" >{"Where can I purchase medical cannabis in Utah?"}</Link>
-                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-4xl text-greenThree" >{"Can I grow my own cannabis plants with a medical cannabis card?"}</Link>
-                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-4xl text-greenThree" >{"How long is a medical cannabis card valid?"}</Link>
-                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-4xl text-greenThree" >{"Is my personal information confidential when applying for a medical cannabis card?"}</Link>
+                <h2 className="text-4xl leading-loose text-greenTwo ">{"FAQs..."}</h2>
+                <div className="flex flex-col gap-4">  
+                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-xl text-greenThree" >{"If I don't qualify, do i still have to pay the QMP?"}</Link>
+                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-xl text-greenThree" >{"What are Utah's qualifying medical conditions?"}</Link>
+                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-xl text-greenThree" >{"Is documentation required to prove you have a medical condition?"}</Link>
+                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-xl text-greenThree" >{"If I have a medical cannabis card, can I consume in public?"}</Link>
+                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-xl text-greenThree" >{"What are the eligiblity requirements for a Utah medical cannabis card?"}</Link>
+                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-xl text-greenThree" >{"What is the complete cost to get a medical cannabis card?"}</Link>
+                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-xl text-greenThree" >{"How long does it take to receive your card?"}</Link>
+                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-xl text-greenThree" >{"Where can I purchase medical cannabis in Utah?"}</Link>
+                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-xl text-greenThree" >{"Can I grow my own cannabis plants with a medical cannabis card?"}</Link>
+                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-xl text-greenThree" >{"How long is a medical cannabis card valid?"}</Link>
+                    <Link href="/FAQs" className="leading-relaxed max-w-prose text-xl text-greenThree" >{"Is my personal information confidential when applying for a medical cannabis card?"}</Link>
                 </div>
             </div>
         </section>
