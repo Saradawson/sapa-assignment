d0c6a572938f719ced7d1bb7756e2795b42a1bd5
unknown <saradawsone@gmail.com>
2024-05-26 20:03:59 -0600

    about page optimized

--- app/pages/about.jsx
+++ app/pages/about.jsx
@@ -2,25 +2,25 @@ import Image from "next/image";
 
 export default function About() {
     return(
-        <section className=" pt-10 pb-20">
-            <div className="flex  justify-between">
-                <div className="flex flex-col gap-20 pt-20">
+        <section className="w-full">
+            <div className="w-full flex sm:flex-col md:flex-col">
+                <div className="lg:w-1/2 flex flex-col justify-around">
                     <div>
-                        <h2 className="text-7xl text-greenTwo leading-normal">{"About Utah Grown"}</h2>
-                        <p className="break-words hyphens-auto max-w-prose text-3xl leading-relaxed text-greyTwo">{"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore."}</p>
+                        <h2 className="text-4xl text-greenTwo leading-normal">{"About Utah Grown"}</h2>
+                        <p className="break-words hyphens-auto max-w-prose text-xl leading-relaxed text-greyTwo">{"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore."}</p>
                     </div>
                     <div>    
-                        <h2 className="text-7xl text-greenTwo leading-normal">{"Utah Grown Office"}</h2> 
-                        <h3 className="text-4xl text-greyTwo leading-10">730 State Street <br/> Salt Lake City, Utah 84111</h3>
+                        <h2 className="text-4xl text-greenTwo leading-normal">{"Utah Grown Office"}</h2> 
+                        <h3 className="text-2xl text-greyTwo leading-10">730 State Street <br/> Salt Lake City, Utah 84111</h3>
                     </div>
                 </div>
-                <div className=" flex justify-end">
+                <div className="lg:w-1/2 flex items-end">
                     <Image
                         src="/assets/NormalizeItUtah@2x.png"
                         alt="normalize Utah"
-                        className="absolute"
-                        width={800}
-                        height={1000}
+                        className="w-full h-auto"
+                        width={1319}
+                        height={1567}
                     />
                 </div>
             </div>
